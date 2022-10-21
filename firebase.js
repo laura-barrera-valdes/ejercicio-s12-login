ea769bc9dcd4c99a24d89441f1312e86b98f76c6
laura-barrera-valdes <laurabv99@gmail.com>
2022-10-21 08:52:33 -0500

    comience a usar firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKbY8J4FDiEDA9vTh-X-YlTbpcCn0nfT0",
+  authDomain: "ejercicio-s12-login.firebaseapp.com",
+  projectId: "ejercicio-s12-login",
+  storageBucket: "ejercicio-s12-login.appspot.com",
+  messagingSenderId: "584545455516",
+  appId: "1:584545455516:web:608ebfa0995ddc002ecc59"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
